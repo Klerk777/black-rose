2efe8de81dbdd707674878d33a27223c48e7253f
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2023-02-01 23:02:17 +0200

    Changes to be committed: 	modified:   src/components/App.jsx 	new file:   "src/components/DALL\302\267E 2023-02-01 21.10.22 - \320\241reate an illustration for the poem below about Black Rose in the style of Stephen King.png" 	new file:   "src/components/DALL\302\267E 2023-02-01 21.10.22 - \320\241reate an illustration for the poem below about Black Rose in the style of Stephen King_dark-bg.png" 	modified:   src/index.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import blackRose from './DALL·E 2023-02-01 21.10.22 - Сreate an illustration for the poem below about Black Rose in the style of Stephen King_dark-bg.png';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="article-box">
+      <article className="article">
+        <img className="img" src={blackRose} alt="Black Rose" />
+        {/* prettier-ignore */}
+        <h1>Black Rose</h1>
+        <p className="text">
+          A black rose blooms in darkness, <br />
+          Its thorns a symbol of pain.
+          <br />
+          It stands alone in shadows,
+          <br />
+          Its beauty both bold and strange.
+          <br />
+          <br />
+          Its petals soft and velvety,
+          <br />
+          Its scent a haunting refrain.
+          <br />
+          Its presence both dark and mystic,
+          <br />
+          A symbol of love's disdain.
+          <br />
+          <p className="author">
+            Writed by Chat GPT with soul of Stephen King <br />
+            Illustration created by DALL-E 2
+          </p>
+        </p>
+      </article>
     </div>
   );
 };
